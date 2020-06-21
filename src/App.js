d270911c187b94c448629aa90210d1e5aa38d348
Filip Grace <filip@filbot.com>
2020-06-21 11:06:12 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+const apiKey = 'DEMO_KEY';
+const endpoint = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`;
+
 function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(endpoint)
+        .then(response => response.json())
+        .then(jsonData => {
+          const {sol_keys, validity_checks, ...solData} = jsonData;
+          return Object.entries(solData).map(([sol, data]) => {
+            return {
+              sol: sol,
+              maxTemp: data.AT.mx,
+              minTemp: data.AT.mn,
+              windSpeed: data.HWS.av,
+              season: data.Season,
+              date: new Date(data.First_UTC)
+            }
+          })
+        })
+      setData(result);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading ? (
+        <div className="loading">
+          <p>--- LOADING ---</p>
+        </div>
+      ) : (
+        <div>
+        <p>{data[0].season} in Elysium Planitia, sol {data[0].sol}</p>
+        <div className="temperature">{data[0].maxTemp}°</div>
+        <div className="wind">Wind Average: {data[0].windSpeed} mph</div>
+        <div className="bottom-data">
+          <div className="highs">High: {data[0].maxTemp}</div>
+          <div className="lows">Low: {data[0].minTemp}</div>
+        </div>
+        </div>
+      )}
     </div>
   );
 }
